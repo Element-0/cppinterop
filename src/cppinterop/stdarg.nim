91569f238747b766fa370a834d24a47e9461ebfa
CodeHz <codehz@outlook.com>
2021-01-29 05:54:15 +0800

    fix vsnprintf unresolved

--- src/cppinterop/stdarg.nim
+++ src/cppinterop/stdarg.nim
@@ -1,7 +1,7 @@
 type
   va_list* {.importc, header: "stdarg.h".} = cstring
 
-proc vsnprintf(buffer: cstring; size: int; fmt: cstring; args: va_list): int {.importc.}
+proc vsnprintf(buffer: cstring; size: int; fmt: cstring; args: va_list): int {.stdcall, importc, dynlib: "msvcrt".}
 
 proc cfmt*(fmt: cstring; args: va_list): string =
   let len = vsnprintf(nil, 0, fmt, args)
